82160a278cf104f7bb64450644e340ecb23af8c3
Nischal Maharjan <burninglykfire@gmail.com>
2023-03-12 16:46:12 +0545

    Deleting unnecessary files

--- backend/base/urls/vendor_urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from backend.base.views.product_views import API
-from base.views import vendor_views as views
-
-
-urlpatterns = [
-    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('register/', views.registerVendor, name='register'),
-] 
